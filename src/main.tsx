9c212ac07d4fdbee5b3b7c3e48e9412d3190e64e
hiroyone <yonetani@team-lab.com>
2022-04-17 16:22:02 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Timeline from "./pages/timeline";
+import Timeline from "./routes/timeline";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

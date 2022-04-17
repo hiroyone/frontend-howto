dcd6c8a0fdc99cbd4a3c9da09e7b777f3a888b5b
hiroyone <yonetani@team-lab.com>
2022-04-17 15:39:12 +0900

    Add timeline

---
+++ src/pages/timeline.tsx
@@ -0,0 +1,67 @@
+import styles from "./timeline.module.scss";
+
+export default function TimelinePage() {
+  return (
+    <>
+      <div className={styles.timeline}>
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            <h2>2017</h2>
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            <h2>2016</h2>
+            <p>Lorem ipsum..</p>
+          </div>
+        </div>
+
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            <h2>2015</h2>
+            <p>
+              Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+              admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+              quis iuvaret expetendis his, te elit voluptua dignissim per, habeo
+              iusto primis ea eam.
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            <h2>2012</h2>
+            <p>
+              Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+              admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+              quis iuvaret expetendis his, te elit voluptua dignissim per, habeo
+              iusto primis ea eam.
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.left}`}>
+          <div className={styles.content}>
+            <h2>2011</h2>
+            <p>
+              Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+              admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+              quis iuvaret expetendis his, te elit voluptua dignissim per, habeo
+              iusto primis ea eam.
+            </p>
+          </div>
+        </div>
+        <div className={`${styles.container} ${styles.right}`}>
+          <div className={styles.content}>
+            <h2>2007</h2>
+            <p>
+              Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec
+              admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea
+              quis iuvaret expetendis his, te elit voluptua dignissim per, habeo
+              iusto primis ea eam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
